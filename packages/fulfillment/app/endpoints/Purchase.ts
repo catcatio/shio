4f241f654bd3c782b3b34939a9a01fa015cbe012
Parinthorn Panya <mai@digithunworldwide.com>
2019-06-15 03:56:21 +0700

    find asset via assetId in Purchase endpoint

--- packages/fulfillment/app/endpoints/Purchase.ts
+++ packages/fulfillment/app/endpoints/Purchase.ts
@@ -1,22 +1,28 @@
 import { endpointFn, EndpointFunctionAncestor } from './default'
 import * as uuid from 'uuid/v4'
-import { PurchaseItemEventMessageIntentKind, ReservePaymentMessage } from '@shio-bot/foundation/entities';
+import { PurchaseItemEventMessageIntentKind, ReservePaymentMessage, AssetMetadataBookKind } from '@shio-bot/foundation/entities'
 
 export const PurchaseItemEventMessageIntentEndpoint = (ancestor: EndpointFunctionAncestor) =>
   endpointFn(PurchaseItemEventMessageIntentKind, async message => {
-    const { merchantTitle } = message.intent.parameters
+    const { assetId } = message.intent.parameters
 
+    const asset = await ancestor.merchandise.findAssetByIdOrThrow(assetId)
     // TODO: create
-    const reservePaymentMessage: ReservePaymentMessage = {
-      type: 'ReservePayment',
-      provider: 'linepay',
-      orderId: uuid(), // primary key of purchasing
-      productName: merchantTitle,
-      productImageUrl: 'https://static.reeeed.com/book/cjn66col600cw08027wemah6s/shareThumbnailImage-medium.jpg', // optional
-      amount: 120, // unit price
-      currency: 'THB',
-      source: message.source
+    const kind = asset.meta.kind
+    if (kind === AssetMetadataBookKind) {
+      const reservePaymentMessage: ReservePaymentMessage = {
+        type: 'ReservePayment',
+        provider: 'linepay',
+        orderId: uuid(), // primary key of purchasing
+        productName: asset.meta.title,
+        productDescription: asset.meta.description,
+        productImageUrl: 'https://static.reeeed.com/book/cjn66col600cw08027wemah6s/shareThumbnailImage-medium.jpg', // optional
+        amount: 120, // unit price
+        currency: 'THB',
+        source: message.source
+      }
+      return reservePaymentMessage
     }
 
-    return reservePaymentMessage
+    return
   })
--- packages/fulfillment/app/usecases/merchandise.ts
+++ packages/fulfillment/app/usecases/merchandise.ts
@@ -1,8 +1,8 @@
-import { PaginationResult } from "@shio-bot/foundation/entities";
-import { ACLRepository, UserRepository, WithOperationOwner, WithPagination, WithSystemOperation, OperationOption, composeOperationOptions } from "../repositories";
-import { AssetRepository } from "../repositories/asset";
-import { Asset } from "../entities/asset";
-
+import { PaginationResult } from '@shio-bot/foundation/entities'
+import { Asset } from '../entities/asset'
+import { ACLRepository, UserRepository, WithOperationOwner, WithPagination, WithSystemOperation, OperationOption, composeOperationOptions } from '../repositories'
+import { AssetRepository } from '../repositories/asset'
+import { ErrorType, newGlobalError } from '../entities/error'
 
 interface MerchandiseListItemInput {
   merchandiseId?: string
@@ -11,10 +11,10 @@ interface MerchandiseListItemInput {
 }
 export interface MerchandiseUseCase {
   listItem(input: MerchandiseListItemInput, ...options: OperationOption[]): Promise<PaginationResult<Asset>>
+  findAssetByIdOrThrow(assetId: string): Promise<Asset>
 }
 
 export class DefaultMerchandiseUseCase implements MerchandiseUseCase {
-
   public Acl: ACLRepository
   public User: UserRepository
   public Asset: AssetRepository
@@ -27,9 +27,15 @@ export class DefaultMerchandiseUseCase implements MerchandiseUseCase {
 
   public async listItem({ limit, merchandiseId, offset }: MerchandiseListItemInput, ...options: OperationOption[]): Promise<PaginationResult<Asset>> {
     const option = composeOperationOptions(...options)
-    return this.Asset.findMany(
-      WithPagination(limit, offset),
-      WithOperationOwner(option.operationOwnerId)
-    )
+    return this.Asset.findMany(WithPagination(limit, offset), WithOperationOwner(option.operationOwnerId))
+  }
+
+  public async findAssetByIdOrThrow(assetId: string): Promise<Asset> {
+    const asset = await this.Asset.findById(assetId)
+    if (!asset) {
+      throw newGlobalError(ErrorType.NotFound, 'asset with provided ID is not found')
+    } else {
+      return asset
+    }
   }
 }
